14fad5fbe53666aa81034c195c81aa53f2c924bb
devTeam222 <ochokom@ochotouchsolution.onmicrosoft.com>
2024-05-14 19:59:00 +0100

    Update index.php and add console messages in theme.js; add copyright notice in style.css
    The index.php file has been updated with an additional import statement for a new configuration file. The theme.js file now includes console messages

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
+
 $root = './';
 require_once('./config/config.php');
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
