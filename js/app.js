6b6ead8d99f468c215085e387790f7b8b7eaed81
devTeam222 <ochokom@ochotouchsolution.onmicrosoft.com>
2024-05-15 15:09:40 +0100

    Update console log messages for copyright information
    The copyright message in the console log has been updated to be displayed on a single line and a second message has been added to display the same information in French.

--- js/app.js
+++ js/app.js
@@ -20,10 +20,8 @@ async function App() {
     console.warn("%cAVERTISSEMENT !\n\n%cExécuter du code dans cette console peut causer des problèmes de sécurité et de performance. Si quelqu'un vous a dit d'entrer quelque chose ici, il y a de fortes chances qu'il essaie de vous tromper et d'accéder à vos informations personnelles. Fermez cette console sauf si vous savez exactement ce que vous faites.", 'color: red; font-size: 30px;', 'font-size: 20px');
 
     // ? Message de copyright
-    console.log(`%cDesigned and implemented by Martin OCHO. 
-    For more information, please visit my GitHub profile: https://github.com/OchoKOM/. 
-    All rights reserved, 2024.`, 'color: dodgerblue; font-size: 15px;');
-    console.log('%cConçu et réalisé par Martin OCHO. Pour plus d\'informations, veuillez visiter mon profil GitHub : https://github.com/OchoKOM/. Tous droits réservés, 2024.', 'color: dodgerblue; font-size: 15px;');
+    console.log(`%cDesigned and implemented by Martin OCHO. \nFor more information, please visit my GitHub profile: https://github.com/OchoKOM/. \nAll rights reserved, 2024.`, 'color: dodgerblue; font-size: 15px;');
+    console.log('%cConçu et réalisé par Martin OCHO. \nPour plus d\'informations, veuillez visiter mon profil GitHub : https://github.com/OchoKOM/. \nTous droits réservés, 2024.', 'color: dodgerblue; font-size: 15px;');
 
 
     if (currentUser.user_id) {
